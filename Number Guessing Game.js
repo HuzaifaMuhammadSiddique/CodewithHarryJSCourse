03112c0e0fd9d5443ce26496b9c75ef436b11afe
Huzaifa Muhammad Siddique <88268541+HuzaifaMuhammadSiddique@users.noreply.github.com>
2023-04-26 02:19:08 +0300

    Update Number Guessing Game.js

--- Number Guessing Game.js
+++ Number Guessing Game.js
@@ -6,6 +6,12 @@ do{
   guess = Number.parseInt(prompt("What is your Guess: "))
   if(number === guess){
     break;
+  } else{
+    if(guess>number){
+      console.log("Try Again! Your guess is greater than the number!");
+    } else if(guess<number){
+      console.log("Try Again! Your guess is less than the number!");
+    }
   }
   lives -= 1;
 }while(number !== guess);
